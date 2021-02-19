40aea32c418d9826358ecec4c7cc70f713bfa944
Juan Pablo Gonzales <juampi7237@gmail.com>
2021-02-19 16:29:32 -0400

    fixed all errors for the checkstyle

--- src/main/java/org/fundacionjala/prog101/ExercicesJP/Calculator.java
+++ src/main/java/org/fundacionjala/prog101/ExercicesJP/Calculator.java
@@ -1,9 +1,13 @@
 package org.fundacionjala.prog101.ExercicesJP;
 
 public class Calculator {
-    public int add(String numbers){
-        if(!numbers.isEmpty()){
-            String[] arrayNumbers = numbers.split("[,]");
+
+    /**
+     * Method for sum numbers into a String .
+     */
+    public int add(final String numbers) {
+        if (!numbers.isEmpty()) {
+            String[] arrayNumbers = numbers.split(",");
             int sum = 0;
             for (String number : arrayNumbers) {
                 sum += Integer.parseInt(number);
