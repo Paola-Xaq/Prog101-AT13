7ec222c35f138c2065b38f834936177ef8a1bacb
edsonarios <edsonrios9@gmail.com>
2021-02-19 15:28:07 -0400

    add gradle 6.x, jacoco coverage and checkstyle

--- src/main/java/Main.java
+++
@@ -1,8 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello Progra 101");
-    }
-    public int divide( int num, int div) {
-        return num / div;
-    }
-}
