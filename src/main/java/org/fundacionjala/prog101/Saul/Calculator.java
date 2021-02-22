a42cc61e37b00affced71cb8929d2c3d83c11658
SaulCM <saulcaspamiranda@gmail.com>
2021-02-22 10:47:52 -0400

    can split by new lines and commas

--- src/main/java/org/fundacionjala/prog101/Saul/Calculator.java
+++ src/main/java/org/fundacionjala/prog101/Saul/Calculator.java
@@ -17,4 +17,18 @@ public class Calculator {
             }
         return sum;
     }
+
+    /**
+     * Method to split a String into an array of int.
+     * @param numbers.
+     * @return the array of int.
+     */
+    public int[] split(final String numbers) {
+        String[]stringRes = numbers.split("\n|,");
+        int[]intRes = new int[stringRes.length];
+        for (int pos = 0; pos < intRes.length; pos++) {
+            intRes[pos] = Integer.parseInt(stringRes[pos]);
+        }
+        return intRes;
+    }
 }
--- src/test/java/org/fundacionajala/prog101/Saul/CalculatorTest.java
+++ src/test/java/org/fundacionajala/prog101/Saul/CalculatorTest.java
@@ -5,14 +5,14 @@ import static org.junit.Assert.*;
 
 public class CalculatorTest {
     @Test
-    public void sum_NumberOnePlusNumberTwo_Three() {
+    public void add_NumberOnePlusNumberTwo_Three() {
         Calculator calculator = new Calculator();
         int expected = 3;
         int actual = calculator.add("1,2");
         assertEquals(expected, actual);
     }
     @Test
-    public void sum_EmptyString_Zero() {
+    public void add_EmptyString_Zero() {
         Calculator calculator = new Calculator();
         int expected = 0;
         int actual = calculator.add("");
@@ -20,7 +20,7 @@ public class CalculatorTest {
     }
 
     @Test
-    public void sum_EmptyNumberAndTwo_Two() {
+    public void add_EmptyNumberAndTwo_Two() {
         Calculator calculator = new Calculator();
         int expected = 2;
         int actual = calculator.add(",2");
@@ -28,10 +28,17 @@ public class CalculatorTest {
     }
 
     @Test
-    public void sum_NumberNegativeTwoAndOne_NegativeOne() {
+    public void add_NumberNegativeTwoAndOne_NegativeOne() {
         Calculator calculator = new Calculator();
         int expected = -1;
         int actual = calculator.add("-2,1");
         assertEquals(expected, actual);
+    }
+    @Test
+    public void split_StringWithNewLines_returnExpectedArray() {
+        Calculator calculator = new Calculator();
+        int[]expected = {1,2,3};
+        int[] actual = calculator.split("1\n2,3");
+        assertArrayEquals(expected, actual);
     }
 }
