6c7385fe4d0e338a20f02cce75de3fbf44dde424
Paola-Xaq <paola.xaq@gmail.com>
2021-02-21 22:40:05 -0400

    Refactor Calculator Class
    Add method getNumbers which separed the numbers of the String and validates the change of delimiter.
    Add Tests for the changing of delimiter.

--- src/main/java/org/fundacionjala/prog101/PaolaAguilar/Calculator.java
+++ src/main/java/org/fundacionjala/prog101/PaolaAguilar/Calculator.java
@@ -2,22 +2,35 @@ package org.fundacionjala.prog101.PaolaAguilar;
 
 public class Calculator {
 
-    private String delimiter = ",";
+    private String delimiter = "\\r?\\n|\\,";
 
     /**
      *
      */
     public int sum(final String numbers) {
+        int sum = 0;
         if (numbers.isEmpty()) {
             return 0;
         }
-        String[] numberString = numbers.split(delimiter);
-        int sum = 0;
-        for (String number: numberString) {
+        String[] numbersArray = getNumbers(numbers);
+        for (String number: numbersArray) {
             if (!(number.length() == 0)) {
                 sum += Integer.parseInt(number);
             }
         }
         return sum;
+    }
+
+    /**
+     *
+     * */
+    public String[] getNumbers(final String numbers) {
+        if (numbers.startsWith("//")) {
+            int inferiorLimit = 2;
+            int superiorLimit = 3;
+            delimiter = "\\r?\\n|\\" + numbers.substring(inferiorLimit, superiorLimit);
+            return numbers.substring(superiorLimit).split(delimiter);
+        }
+        return numbers.split(delimiter);
     }
 }
--- src/test/java/org/fundacionjala/prog101/PaolaAguilar/CalculatorTest.java
+++ src/test/java/org/fundacionjala/prog101/PaolaAguilar/CalculatorTest.java
@@ -7,14 +7,6 @@ import static org.junit.Assert.*;
 public class CalculatorTest {
 
     @Test
-    public void sum_NumberOnePlusNumberTwo_Three() {
-        Calculator calculator = new Calculator();
-        int expected = 3;
-        int actual = calculator.sum("1,2");
-        assertEquals(expected, actual);
-    }
-
-    @Test
     public void sum_EmptyString_Zero() {
         Calculator calculator = new Calculator();
         int expected = 0;
@@ -23,10 +15,58 @@ public class CalculatorTest {
     }
 
     @Test
+    public void sum_OnlyNumberOne_One() {
+        Calculator calculator = new Calculator();
+        int expected = 1;
+        int actual = calculator.sum("1");
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void sum_NumberOnePlusNumberTwo_Three() {
+        Calculator calculator = new Calculator();
+        int expected = 3;
+        int actual = calculator.sum("1,2");
+        assertEquals(expected, actual);
+    }
+
+    @Test
     public void sum_EmptyNumberAndTwo_Two() {
         Calculator calculator = new Calculator();
         int expected = 2;
         int actual = calculator.sum(",2");
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void sum_TwoAndEmptyNumber_Two() {
+        Calculator calculator = new Calculator();
+        int expected = 2;
+        int actual = calculator.sum("2,");
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void sum_OneNewLineTwoPlusTwo_Five() {
+        Calculator calculator = new Calculator();
+        int expected = 5;
+        int actual = calculator.sum("1\n2,2");
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void sum_OnePlusTwoNewLineTwo_Five() {
+        Calculator calculator = new Calculator();
+        int expected = 5;
+        int actual = calculator.sum("1,2\n2");
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void sum_OnePlusTwoChangingDelimiter_Three() {
+        Calculator calculator = new Calculator();
+        int expected = 3;
+        int actual = calculator.sum("//;\n1;2");
         assertEquals(expected, actual);
     }
 
