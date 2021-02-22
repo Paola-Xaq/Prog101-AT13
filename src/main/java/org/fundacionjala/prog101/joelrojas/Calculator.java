e88a441e84c206f9db25080d5433f0381bf3981e
Joel Rojas <jr.bestone17@gmail.com>
2021-02-21 21:45:05 -0400

    Add comments to every method and clean code

--- src/main/java/org/fundacionjala/prog101/joelrojas/Calculator.java
+++ src/main/java/org/fundacionjala/prog101/joelrojas/Calculator.java
@@ -6,33 +6,41 @@ public class Calculator {
     public static final String COMMA = ",";
     public static final String NEXT_LINE = "\n";
 
-    /** */
+    /**
+     * @return an integer that is the total sum.
+    */
     public int sum(String numbers) {
         if (numbers.isEmpty()) {
             return 0;
         } else if (!numbers.startsWith("//")) {
-            return sumWoD(numbers);
+            return sumWithoutDelimiters(numbers);
         } else if (!numbers.startsWith("//[")) {
-            return sumWASD(numbers);
+            return sumWithASimpleDelimiter(numbers);
         } else {
-            return sumWDD(numbers);
+            return sumWithDifferentDelimiters(numbers);
         }
     }
 
-    /** */
-    public int sumWoD(String numbers) {
+    /**
+     * @return an integer that is the sum from a input without delimiter(s).
+    */
+    public int sumWithoutDelimiters(String numbers) {
         return sumIntArray(separateNumberString(numbers));
     }
 
-    /** */
-    public int sumWASD(String numbers) {
+    /**
+     * @return an integer that is the sum from a input with a simple delimiter.
+    */
+    public int sumWithASimpleDelimiter(String numbers) {
         String delimiter = numbers.substring(2, numbers.indexOf("\n"));
         String newNumbers = cleanNumbers(numbers);
         return sumIntArray(separateNumberString(newNumbers, delimiter));
     }
 
-    /** */
-    public int sumWDD(String numbers) {
+    /**
+     * @return an integer that is the sum from a input with different delimiter(s).
+    */
+    public int sumWithDifferentDelimiters(String numbers) {
         String delimiters = numbers.substring(POS_INI_DELIMITER, numbers.indexOf("\n"));
         String[] delimitersListos = obtaindelimiter(delimiters);
         String newNumbers = cleanNumbers(numbers);
@@ -53,12 +61,15 @@ public class Calculator {
         return inputNumber < 0;
     }
 
-    /** */
+    /**
+     * @return a string without the delimiter(s).
+    */
     public String cleanNumbers(String numbers) {
         return numbers.substring(numbers.indexOf("\n") + 1);
     }
+
     /**
-     *
+     * @return a string array that contains every separated delimiter.
      */
     public String[] obtaindelimiter(String delimiters) {
         int closedBracket = 0;
@@ -76,20 +87,23 @@ public class Calculator {
         } while (!esta);
         return delimitersWithSpaces.split("\n");
     }
+
     /**
-     *
+     * @return a string array where they are every number as strings that before they were separated by commas.
      */
     public String[] separateNumberString(String numberString) {
         return separateNumberString(convertToOneDelimiter(numberString), COMMA);
     }
+
     /**
-     *
+     * @return a string array where they are every number as strings that before they were separated by some delimiter.
      */
     public String[] separateNumberString(String numberString, String delimiter) {
         return numberString.split("\\" + delimiter);
     }
+
     /**
-     *
+     * @return a string array where they are every number as strings that before they were separated by some delimiters.
      */
     public String[] separateNumberString(String numberString, String[] delimiters) {
         String numStrings1 = numberString;
@@ -99,17 +113,22 @@ public class Calculator {
         return separateNumberString(numStrings1);
     }
 
-    /** */
+    /**
+     * @return a string where the next lines were replaced by commas.
+    */
     public String convertToOneDelimiter(String numbers) {
         return numbers.replaceAll(NEXT_LINE, COMMA);
     }
-    /** */
+
+    /**
+     * @return a string where the delimiters were replaced by commas.
+    */
     public String convertToOneDelimiter(String numbers, String delimiter) {
         return numbers.replaceAll(delimiter, COMMA);
     }
 
     /**
-     * sum every number inside the array.
+     * @return an integer that is sum of all these string number converted to integer number.
     */
     public int sumIntArray(String[] stringNumbers) {
         int sum = 0;
