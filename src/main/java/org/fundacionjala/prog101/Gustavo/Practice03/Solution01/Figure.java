7d5ba0de2d23c3cee24448c257a57a098136fb6c
Gustavo Huanca <gustavozha@gmail.com>
2021-03-02 17:47:51 -0400

    New class Rectangle + it's methods

--- src/main/java/org/fundacionjala/prog101/Gustavo/Practice03/Solution01/Figure.java
+++ src/main/java/org/fundacionjala/prog101/Gustavo/Practice03/Solution01/Figure.java
@@ -1,4 +1,12 @@
 package org.fundacionjala.prog101.Gustavo.Practice03.Solution01;
 
 public class Figure {
+    String Tipo;
+
+    public double getArea() {
+        return 0;
+    }
+
+    public void draw() {
+    }
 }
---
+++ src/main/java/org/fundacionjala/prog101/Gustavo/Practice03/Solution01/Rectangle.java
@@ -0,0 +1,25 @@
+package org.fundacionjala.prog101.Gustavo.Practice03.Solution01;
+
+public class Rectangle {
+    private int high;
+    private int base;
+    Rectangle(int high, int base){
+        this.high=high;
+        this.base=base;
+    }
+    public double getArea(){
+        return high*base;
+    }
+
+    public void drawn(){
+        for (int i = 0; i < high; i++)
+        {
+            for (int j = 0; j < base; j++)
+            {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
+}
