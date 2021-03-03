b9dc2d49c192469b469a36aba4d587ddffd32415
Gustavo Huanca <gustavozha@gmail.com>
2021-03-02 23:54:18 -0400

    Change FigurePlane class Abstract to aggregatrion and implement in Rectangle class

--- src/main/java/org/fundacionjala/prog101/Gustavo/Practice03/Solution03/Rectangle.java
+++ src/main/java/org/fundacionjala/prog101/Gustavo/Practice03/Solution03/Rectangle.java
@@ -1,9 +1,18 @@
 package org.fundacionjala.prog101.Gustavo.Practice03.Solution03;
 
-public class Rectangle extends FigurePlane {
+public class Rectangle extends Figure {
     private int high;
     private int base;
+    FigurePlane figurePlane;
     Rectangle(int high, int base){
-        super( high, base);
+        this.base=base;
+        this.high=high;
+        figurePlane=new FigurePlane(base,high);
+    }
+    public double getArea(){
+        return figurePlane.getArea();
+    }
+    public void drawn(){
+        figurePlane.drawn();
     }
 }
