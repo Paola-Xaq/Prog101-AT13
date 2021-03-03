131299db9f0fc110425c5bc36edaffe09525df65
Juan Pablo Gonzales <juampi7237@gmail.com>
2021-03-02 22:38:40 -0400

    fix message in solution 2

--- src/main/java/org/fundacionjala/prog101/ExercicesJP/solution3/Main.java
+++ src/main/java/org/fundacionjala/prog101/ExercicesJP/solution3/Main.java
@@ -3,7 +3,7 @@ package org.fundacionjala.prog101.ExercicesJP.solution3;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Solucion 3");
-        Figura rectangle = new Rectangulo(4,3);
+        Figura rectangle = new Rectangulo(4,2);
         Figura square = new Cuadrado(4);
         rectangle.dibujar();
         System.out.println(rectangle.getArea());
