a1e51110e1f09ea264493a0235afaa9329a5dfdc
Gustavo Huanca <gustavozha@gmail.com>
2021-03-02 22:34:28 -0400

    Add method getArea for Circle class

--- src/main/java/org/fundacionjala/prog101/Gustavo/Practice03/Solution01/Circle.java
+++ src/main/java/org/fundacionjala/prog101/Gustavo/Practice03/Solution01/Circle.java
@@ -1,4 +1,14 @@
 package org.fundacionjala.prog101.Gustavo.Practice03.Solution01;
 
 public class Circle {
+    int radio;
+
+    public Circle(int radius)
+    {
+        this.radio = radius;
+    }
+    public  double getArea()
+    {
+        return Math.PI * Math.pow(radio, 2);
+    }
 }
