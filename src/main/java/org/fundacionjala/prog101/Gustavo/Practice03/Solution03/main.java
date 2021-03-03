e4ca1b82f1b09c90a6bf33634c40d6a5cc14b868
Gustavo Huanca <gustavozha@gmail.com>
2021-03-02 23:38:29 -0400

    New main class and file Solution03

---
+++ src/main/java/org/fundacionjala/prog101/Gustavo/Practice03/Solution03/main.java
@@ -0,0 +1,11 @@
+package org.fundacionjala.prog101.Gustavo.Practice03.Solution03;
+
+import org.fundacionjala.prog101.Gustavo.Practice03.Solution02.*;
+
+import java.util.ArrayList;
+
+public class main {
+    public static void main(String[] args) {
+
+    }
+}
