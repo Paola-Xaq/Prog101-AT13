c66ee22e1915cb0100e40d17d0ae8766b647e8a1
Gustavo Huanca <gustavozha@gmail.com>
2021-03-02 22:44:26 -0400

    Add getArea method for Rhombus class

--- src/main/java/org/fundacionjala/prog101/Gustavo/Practice03/Solution01/Rhombus.java
+++ src/main/java/org/fundacionjala/prog101/Gustavo/Practice03/Solution01/Rhombus.java
@@ -1,4 +1,15 @@
 package org.fundacionjala.prog101.Gustavo.Practice03.Solution01;
 
 public class Rhombus {
+    int wide;
+    int high;
+    public Rhombus(int high, int wide)
+    {
+        this.wide = wide;
+        this.high = high;
+    }
+    public double getArea()
+    {
+        return (wide * high) / 2;
+    }
 }
