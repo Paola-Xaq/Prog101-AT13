f4d9a01777a7c6baa58b98f84af606f23910360b
Joel Rojas <jr.bestone17@gmail.com>
2021-03-07 19:36:32 -0400

    Add Main class to diamondInterfaces package and change variable's names of GameObject class

--- src/main/java/org/fundacionjala/prog101/joelrojas/game/GameObject.java
+++ src/main/java/org/fundacionjala/prog101/joelrojas/game/GameObject.java
@@ -1,28 +1,28 @@
 package org.fundacionjala.prog101.joelrojas.game;
 
 public class GameObject implements VisibleDelegate, MovableDelegate, CollidableDelegate{
-    VisibleDelegate visibleDelegate;
-    MovableDelegate movableDelegate;
-    CollidableDelegate collidableDelegate;
+    private VisibleDelegate visible;
+    private MovableDelegate movable;
+    public CollidableDelegate collidable;
 
     public GameObject(VisibleDelegate v, MovableDelegate m, CollidableDelegate c) {
-        visibleDelegate = v;
-        movableDelegate = m;
-        collidableDelegate = c;
+        visible = v;
+        movable = m;
+        collidable = c;
     }
 
     @Override
     public void collide() {
-        collidableDelegate.collide();
+        collidable.collide();
     }
 
     @Override
     public void move() {
-        movableDelegate.move();
+        movable.move();
     }
 
     @Override
     public void draw() {
-        visibleDelegate.draw();
+        visible.draw();
     }
 }
