a6ccc6bc0638fec0d1859268d2bca90c56e8b538
Gustavo Huanca <gustavozha@gmail.com>
2021-03-07 17:44:35 -0400

    Add draw method in Visible class because Visible class implements VsisibleDelegate

--- src/main/java/org/fundacionjala/prog101/Gustavo/Practice04/composition/Visible.java
+++ src/main/java/org/fundacionjala/prog101/Gustavo/Practice04/composition/Visible.java
@@ -1,4 +1,8 @@
 package org.fundacionjala.prog101.Gustavo.Practice04.composition;
 
-public class Visible{
+public class Visible implements VisibleDelegate{
+    @Override
+    public void draw() {
+        System.out.println("I am drawing ");
+    }
 }
