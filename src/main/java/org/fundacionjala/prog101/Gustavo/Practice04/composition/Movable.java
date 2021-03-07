a7bb7b1472f7b733a1800b3cedcce9cde701f474
Gustavo Huanca <gustavozha@gmail.com>
2021-03-07 17:53:34 -0400

    Add move method in Movable class because Movable class implements MovableDelegate

--- src/main/java/org/fundacionjala/prog101/Gustavo/Practice04/composition/Movable.java
+++ src/main/java/org/fundacionjala/prog101/Gustavo/Practice04/composition/Movable.java
@@ -1,4 +1,8 @@
 package org.fundacionjala.prog101.Gustavo.Practice04.composition;
 
-public class Movable {
+public class Movable implements MovableDelegate{
+    @Override
+    public void move() {
+        System.out.println("I move");
+    }
 }
