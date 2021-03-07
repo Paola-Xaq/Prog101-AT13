7b860c19e418032e64dc647f5e9f949ddda8a9aa
Gustavo Huanca <gustavozha@gmail.com>
2021-03-07 17:51:43 -0400

    Add move method in NotMovable class because NotMovable class implements MovableDelegate

--- src/main/java/org/fundacionjala/prog101/Gustavo/Practice04/composition/NotMovable.java
+++ src/main/java/org/fundacionjala/prog101/Gustavo/Practice04/composition/NotMovable.java
@@ -1,4 +1,9 @@
 package org.fundacionjala.prog101.Gustavo.Practice04.composition;
 
-public class NotMovable {
+public class NotMovable implements MovableDelegate{
+    @Override
+    public void move() {
+        System.out.println("I can't move");
+    }
 }
+ 
