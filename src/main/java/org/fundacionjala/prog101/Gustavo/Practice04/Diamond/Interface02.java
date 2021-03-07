17567d541ad20ed585125f48ad3baa42e717c928
Gustavo Huanca <gustavozha@gmail.com>
2021-03-07 19:15:46 -0400

    Add method01 because Interface02 interface extends Inteface01

--- src/main/java/org/fundacionjala/prog101/Gustavo/Practice04/Diamond/Interface02.java
+++ src/main/java/org/fundacionjala/prog101/Gustavo/Practice04/Diamond/Interface02.java
@@ -1,4 +1,6 @@
 package org.fundacionjala.prog101.Gustavo.Practice04.Diamond;
 
-public interface Interface02 {
+public interface Interface02 extends Interface01 {
+    @Override
+    void method1();
 }
