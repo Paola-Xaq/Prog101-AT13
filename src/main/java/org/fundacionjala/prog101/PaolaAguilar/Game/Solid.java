db0829e2312ee69cd01d1fb265336c49133f1e3b
Paola X. Aguilar Quiñones <paola.xaq@gmail.com>
2021-03-16 00:01:34 -0400

    Delete Solid.java

--- src/main/java/org/fundacionjala/prog101/PaolaAguilar/Game/Solid.java
+++
@@ -1,8 +0,0 @@
-package org.fundacionjala.prog101.PaolaAguilar.Game;
-
-public class Solid implements CollidableDelegate{
-    @Override
-    public void collide() {
-        System.out.println("Poww - capum.");
-    }
-}
