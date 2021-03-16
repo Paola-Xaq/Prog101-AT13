f5f5ac90360c9a6a97ac06ed76e42dbe052c6203
Paola X. Aguilar Quiñones <paola.xaq@gmail.com>
2021-03-15 23:58:09 -0400

    Delete Invisible.java

--- src/main/java/org/fundacionjala/prog101/PaolaAguilar/Game/Invisible.java
+++
@@ -1,8 +0,0 @@
-package org.fundacionjala.prog101.PaolaAguilar.Game;
-
-public class Invisible implements VisibleDelegate{
-    @Override
-    public void draw() {
-        System.out.println("I cannot, I'm invisible...");
-    }
-}
