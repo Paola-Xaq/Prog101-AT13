51aa6ec9506c05ef865ed84d612e92acf67d7cd7
Paola X. Aguilar Quiñones <paola.xaq@gmail.com>
2021-03-15 23:59:19 -0400

    Delete Movable.java

--- src/main/java/org/fundacionjala/prog101/PaolaAguilar/Game/Movable.java
+++
@@ -1,8 +0,0 @@
-package org.fundacionjala.prog101.PaolaAguilar.Game;
-
-public class Movable implements MovableDelegate{
-    @Override
-    public void move() {
-        System.out.println("I'm moving.");
-    }
-}
