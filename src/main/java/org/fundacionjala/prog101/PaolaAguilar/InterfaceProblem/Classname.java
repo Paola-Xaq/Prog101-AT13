8ce74581e3de634b539011b3d6e503088c4e8684
Paola X. Aguilar Quiñones <paola.xaq@gmail.com>
2021-03-16 00:01:16 -0400

    Delete Classname.java

--- src/main/java/org/fundacionjala/prog101/PaolaAguilar/InterfaceProblem/Classname.java
+++
@@ -1,9 +0,0 @@
-package org.fundacionjala.prog101.PaolaAguilar.InterfaceProblem;
-
-public class Classname implements Interface2, Interface3{
-
-    public String method1(){
-        return Interface2.super.method1() + "\n"
-                + Interface3.super.method1();
-    }
-}
