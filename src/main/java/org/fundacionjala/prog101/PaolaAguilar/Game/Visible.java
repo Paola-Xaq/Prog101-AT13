1a5e6da7b24d2a21adcbfb3f7064605115c5b4ed
Paola X. Aguilar Quiñones <paola.xaq@gmail.com>
2021-03-16 00:01:52 -0400

    Delete Visible.java

--- src/main/java/org/fundacionjala/prog101/PaolaAguilar/Game/Visible.java
+++
@@ -1,8 +0,0 @@
-package org.fundacionjala.prog101.PaolaAguilar.Game;
-
-public class Visible implements VisibleDelegate{
-    @Override
-    public void draw() {
-        System.out.println("I'm drawing...");
-    }
-}
