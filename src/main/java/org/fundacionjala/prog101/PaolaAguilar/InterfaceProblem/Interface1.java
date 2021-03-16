1aeb210921318d55fb46f9769859fa9f21d58fd8
Paola X. Aguilar Quiñones <paola.xaq@gmail.com>
2021-03-16 00:00:36 -0400

    Delete Interface1.java

--- src/main/java/org/fundacionjala/prog101/PaolaAguilar/InterfaceProblem/Interface1.java
+++
@@ -1,7 +0,0 @@
-package org.fundacionjala.prog101.PaolaAguilar.InterfaceProblem;
-
-public interface Interface1 {
-    default String method1() {
-        return "Method1 from Interface1";
-    }
-}
