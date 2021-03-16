79af0e06a6d65a7af6a9a75ad7a727b9e2bbac6e
Paola X. Aguilar Quiñones <paola.xaq@gmail.com>
2021-03-15 23:58:30 -0400

    Delete Main.java

--- src/main/java/org/fundacionjala/prog101/PaolaAguilar/InterfaceProblem/Main.java
+++
@@ -1,8 +0,0 @@
-package org.fundacionjala.prog101.PaolaAguilar.InterfaceProblem;
-
-public class Main {
-    public static void main(String[] args) {
-        Classname classInterface = new Classname();
-        System.out.println(classInterface.method1());
-    }
-}
