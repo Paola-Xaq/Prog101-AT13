175ef52d0d928737bd3c6267d1d812af8fc868ce
Paola X. Aguilar Quiñones <paola.xaq@gmail.com>
2021-03-16 00:00:13 -0400

    Delete Interface2.java

--- src/main/java/org/fundacionjala/prog101/PaolaAguilar/InterfaceProblem/Interface2.java
+++
@@ -1,8 +0,0 @@
-package org.fundacionjala.prog101.PaolaAguilar.InterfaceProblem;
-
-public interface Interface2 extends Interface1{
-    @Override
-    default String method1() {
-        return "Method1 from Interface2";
-    }
-}
