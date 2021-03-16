f4621e8ddf4139a782305569761df5a3cf5a7f33
Paola X. Aguilar Quiñones <paola.xaq@gmail.com>
2021-03-15 23:59:49 -0400

    Delete NotSolid.java

--- src/main/java/org/fundacionjala/prog101/PaolaAguilar/Game/NotSolid.java
+++
@@ -1,8 +0,0 @@
-package org.fundacionjala.prog101.PaolaAguilar.Game;
-
-public class NotSolid implements CollidableDelegate{
-    @Override
-    public void collide() {
-        System.out.println("Splash");
-    }
-}
