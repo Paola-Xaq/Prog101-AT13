5120dd84e1f0753d20ad6a59f3b6493193a7bff4
Paola X. Aguilar Quiñones <paola.xaq@gmail.com>
2021-03-15 23:57:11 -0400

    Delete Interface3.java

--- src/main/java/org/fundacionjala/prog101/PaolaAguilar/InterfaceProblem/Interface3.java
+++
@@ -1,8 +0,0 @@
-package org.fundacionjala.prog101.PaolaAguilar.InterfaceProblem;
-
-public interface Interface3 extends Interface1{
-    @Override
-    default String method1() {
-        return "Method1 from Interface3";
-    }
-}
